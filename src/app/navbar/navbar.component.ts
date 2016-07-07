5d98d4b027d07e69f27402c7c877f3b4eaba34b9
Marty <marsmith@usgs.gov>
2016-07-07 12:36:00 -0400

    implement about modal, remove ng2-bootstrap

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,13 +1,17 @@
-import {Component, Renderer, Inject} from '@angular/core';
+import {Component, Renderer, Inject, ViewChild} from '@angular/core';
 import {DOCUMENT} from '@angular/platform-browser';
+import { MODAL_DIRECTIVES, ModalComponent } from 'ng2-bs3-modal/ng2-bs3-modal';
 
 @Component({
     selector: 'fev-navbar',
+    directives: [MODAL_DIRECTIVES],
     templateUrl: './app/navbar/navbar.component.html'
 })
 export class NavbarComponent {
    appTitle: string = "Flood Event Viewer";
    appTitleMobile: string = "FEV";
+   @ViewChild('modal')
+   modal: ModalComponent;
 
    public isShown:boolean = false;
 
@@ -25,6 +29,13 @@ export class NavbarComponent {
             this.renderer.setElementProperty(this.document.body, 'scrollTop', 0);
         }
         }
+    }
+
+    modalOpen() {
+        this.modal.open();
+    }
+    modalClose() {
+        this.modal.close();
     }
 
 }
