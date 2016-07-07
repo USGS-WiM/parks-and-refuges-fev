999154ca7a8fe017efd1075607b9b037a3e25986
Marty <marsmith@usgs.gov>
2016-07-07 12:38:38 -0400

    remove references to ng2-bootstrap

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,12 +1,9 @@
 import {Component} from '@angular/core';
-import {BUTTON_DIRECTIVES} from 'ng2-bootstrap/components/buttons';
 
 @Component({
     selector: 'fev-sidebar',
-    templateUrl: './app/sidebar/sidebar.component.html',
-    directives: [BUTTON_DIRECTIVES]
+    templateUrl: './app/sidebar/sidebar.component.html'
 })
 export class SidebarComponent {
   public title: string = "Sidebar";
-  public singleModel:string = '1';
 }
