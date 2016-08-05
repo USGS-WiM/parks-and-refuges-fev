d47fca593fa04d01ef7656c243f836db4b9d3ef7
Marty <marsmith@usgs.gov>
2016-08-05 13:31:52 -0400

    test out select2 ajax, show filters in sidebar

--- src/scripts/query.js
+++ src/scripts/query.js
@@ -10,26 +10,43 @@ var buildQueryStrings =  function  () {
         if ($('#evtTypeSelect').val() !== null){
             var evtTypeSelectionsArray = $('#evtTypeSelect').val();
             eventTypeSelections = evtTypeSelectionsArray.toString();
+
+            //update span
+            $('#eventTypeDisplay').html($('#evtTypeSelect').select2('data').map(function(elem){ return elem.text;}).join(', '));
         }
         //event
         var eventSelections = "";
         if ($('#evtSelect').val() !== null){
             var eventSelectionsArray = $('#evtSelect').val();
+            console.log(eventSelectionsArray);
             eventSelections = eventSelectionsArray.toString();
+            
+            //update span
+            $('#eventNameDisplay').html($('#evtSelect').select2('data').map(function(elem){ return elem.text;}).join(', '));
+    
         }
         //event status
         var eventStatusSelectionArray = [];
         //event status: active
         if ($("#active")[0].checked && !($("#complete")[0].checked) ) {
             eventStatusSelectionArray.push(1);
+            $('#eventStatusDisplay').html('Active');
         }
         //event status: complete
         if ($("#complete")[0].checked && !($("#active")[0].checked)) {
             eventStatusSelectionArray.push(2);
+            $('#eventStatusDisplay').html('Complete');
         }
-        if ($("#active")[0].checked && $("#complete")[0].checked || !($("#active")[0].checked) && !($("#complete")[0].checked)) {
+        if ($("#active")[0].checked && $("#complete")[0].checked) {
             eventStatusSelectionArray.push(0);
+            $('#eventStatusDisplay').html('Active, Complete');
         }
+        if ( !($("#active")[0].checked) && !($("#complete")[0].checked)) {
+            eventStatusSelectionArray.push(0);
+             $('#eventStatusDisplay').html('');
+        }
+
+
         var eventStatusSelection =  eventStatusSelectionArray.toString();
 
         //state
@@ -37,12 +54,14 @@ var buildQueryStrings =  function  () {
         if ($('#stateSelect').val() !== null){
             var stateSelectionsArray = $('#stateSelect').val();
             stateSelections = stateSelectionsArray.toString();
+            $('#stateDisplay').html(stateSelections);
         }
         //county
         var countySelections = "";
         if ($('#countySelect').val() !== null){
             var countySelectionsArray = $('#countySelect').val();
             countySelections = countySelectionsArray.toString();
+            $('#countyDisplay').html(countySelections);
         }
 
         ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -53,12 +72,14 @@ var buildQueryStrings =  function  () {
             if ($('#sensorTypeSelect').val() !== null ){
                 var sensorTypeSelectionArray = $('#sensorTypeSelect').val();
                 sensorTypeSelections = sensorTypeSelectionArray.toString();
+                $('#sensorTypeDisplay').html($('#sensorTypeSelect').select2('data')[0].text);
             }
             //sensor status
             var sensorStatusSelections = "";
             if ($('#sensorStatusSelect').val() !== null ){
                 var sensorStatusSelectionArray = $('#sensorStatusSelect').val();
                 sensorStatusSelections = sensorStatusSelectionArray.toString();
+                $('#sensorStatusDisplay').html($('#sensorStatusSelect').select2('data')[0].text);
             }
 
             //sensor collection condition
@@ -66,6 +87,7 @@ var buildQueryStrings =  function  () {
             if ($('#collectionConditionSelect').val() !== null ){
                 var collectConditionSelectionArray = $('#collectionConditionSelect').val();
                 collectConditionSelections = collectConditionSelectionArray.toString();
+                $('#collectConditionDisplay').html($('#collectionConditionSelect').select2('data')[0].text);
             }
 
             //sensor deployment type
@@ -73,17 +95,18 @@ var buildQueryStrings =  function  () {
             if ($('#deployTypeSelect').val() !== null ){
                 var deploymentTypeSelectionArray = $('#deployTypeSelect').val();
                 deploymentTypeSelections = deploymentTypeSelectionArray.toString();
+                $('#deployTypeDisplay').html($('#deployTypeSelect').select2('data')[0].text);
             }
 
-            stnDataPortal.globals.xmlSensorsURLRoot = "http://stn.wim.usgs.gov/STNServices/Instruments";
-            stnDataPortal.globals.jsonSensorsURLRoot = "http://stn.wim.usgs.gov/STNServices/Instruments.json";
-            stnDataPortal.globals.csvSensorsURLRoot = "http://stn.wim.usgs.gov/STNServices/Instruments.csv";
-            stnDataPortal.globals.sensorsQueryString = "?Event=" + eventSelections + "&EventType=" + eventTypeSelections + "&EventStatus=" + eventStatusSelection + "&States=" + stateSelections + "&County=" + countySelections + "&CurrentStatus=" + sensorStatusSelections + "&CollectionCondition=" + collectConditionSelections + "&DeploymentType=" + deploymentTypeSelections;
-            //var resultIsEmpty = false;
+            // stnDataPortal.globals.xmlSensorsURLRoot = "http://stn.wim.usgs.gov/STNServices/Instruments";
+            // stnDataPortal.globals.jsonSensorsURLRoot = "http://stn.wim.usgs.gov/STNServices/Instruments.json";
+            // stnDataPortal.globals.csvSensorsURLRoot = "http://stn.wim.usgs.gov/STNServices/Instruments.csv";
+            // stnDataPortal.globals.sensorsQueryString = "?Event=" + eventSelections + "&EventType=" + eventTypeSelections + "&EventStatus=" + eventStatusSelection + "&States=" + stateSelections + "&County=" + countySelections + "&CurrentStatus=" + sensorStatusSelections + "&CollectionCondition=" + collectConditionSelections + "&DeploymentType=" + deploymentTypeSelections;
+            // //var resultIsEmpty = false;
 
-            stnDataPortal.globals.csvQueryURL = stnDataPortal.globals.csvSensorsURLRoot + stnDataPortal.globals.sensorsQueryString;
-            stnDataPortal.globals.jsonQueryURL = stnDataPortal.globals.jsonSensorsURLRoot + stnDataPortal.globals.sensorsQueryString;
-            stnDataPortal.globals.xmlQueryURL = stnDataPortal.globals.xmlSensorsURLRoot + stnDataPortal.globals.sensorsQueryString;
+            // stnDataPortal.globals.csvQueryURL = stnDataPortal.globals.csvSensorsURLRoot + stnDataPortal.globals.sensorsQueryString;
+            // stnDataPortal.globals.jsonQueryURL = stnDataPortal.globals.jsonSensorsURLRoot + stnDataPortal.globals.sensorsQueryString;
+            // stnDataPortal.globals.xmlQueryURL = stnDataPortal.globals.xmlSensorsURLRoot + stnDataPortal.globals.sensorsQueryString;
 
         }
         ////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -94,22 +117,26 @@ var buildQueryStrings =  function  () {
             if ($('#hwmTypeSelect').val() !== null ){
                 var hwmTypeSelectionArray = $('#hwmTypeSelect').val();
                 hwmTypeSelections = hwmTypeSelectionArray.toString();
+                $('#hwmTypeDisplay').html($('#hwmTypeSelect').select2('data')[0].text);
             }
             //HWM quality
             var hwmQualitySelections = "";
             if ($('#hwmQualitySelect').val() !== null ){
                 var hwmQualitySelectionArray = $('#hwmQualitySelect').val();
                 hwmQualitySelections = hwmQualitySelectionArray.toString();
+                $('#hwmQualityDisplay').html($('#hwmQualitySelect').select2('data')[0].text);
             }
             ////HWM environment
             var hwmEnvSelectionArray = [];
             //HWM environment: coastal
             if ($("#coastal")[0].checked && !($("#riverine")[0].checked)) {
                 hwmEnvSelectionArray.push("Coastal");
+                $('#hwmEnvDisplay').html('Coastal');
             }
             //HWM environment: riverine
             if ($("#riverine")[0].checked && !($("#coastal")[0].checked) ) {
                 hwmEnvSelectionArray.push("Riverine");
+                $('#hwmEnvDisplay').html('Riverine');
             }
             var hwmEnvSelections = hwmEnvSelectionArray.toString();
             //HWM survey status
@@ -117,10 +144,12 @@ var buildQueryStrings =  function  () {
             ///HWM survey status: complete
             if ($("#surveyCompleteYes")[0].checked && !($("#surveyCompleteNo")[0].checked)) {
                 hwmSurveyStatusSelectionArray.push("true");
+                $('#hwmSurveyCompDisplay').html('True');
             }
             ///HWM survey status: not complete
             if ($("#surveyCompleteNo")[0].checked && !($("#surveyCompleteYes")[0].checked)) {
                 hwmSurveyStatusSelectionArray.push("false");
+                $('#hwmSurveyCompDisplay').html('False');
             }
             var hwmSurveyStatusSelections = hwmSurveyStatusSelectionArray.toString();
             //HWM stillwater status
@@ -128,22 +157,24 @@ var buildQueryStrings =  function  () {
             ///HWM stillwater status: yes
             if ($("#stillWaterYes")[0].checked && !($("#stillWaterNo")[0].checked)) {
                 hwmStillwaterStatusSelectionArray.push("true");
+                $('#hwmStillWaterDisplay').html('True');
             }
             ///HWM stillwater status: no
             if ($("#stillWaterNo")[0].checked  && !($("#stillWaterYes")[0].checked)) {
                 hwmStillwaterStatusSelectionArray.push("false");
+                $('#hwmStillWaterDisplay').html('False');
             }
             var hwmStillwaterStatusSelections = hwmStillwaterStatusSelectionArray.toString();
 
-            stnDataPortal.globals.xmlHWMsURLRoot = "http://stn.wim.usgs.gov/STNServices/HWMs/FilteredHWMs";
-            stnDataPortal.globals.jsonHWMsURLRoot = "http://stn.wim.usgs.gov/STNServices/HWMs/FilteredHWMs.json";
-            stnDataPortal.globals.csvHWMsURLRoot = "http://stn.wim.usgs.gov/STNServices/HWMs/FilteredHWMs.csv";
-            stnDataPortal.globals.hwmsQueryString = "?Event=" + eventSelections + "&EventType=" + eventTypeSelections + "&EventStatus=" + eventStatusSelection + "&States=" + stateSelections + "&County=" + countySelections + "&HWMType=" + hwmTypeSelections + "&HWMQuality=" + hwmQualitySelections + "&HWMEnvironment=" + hwmEnvSelections + "&SurveyComplete=" + hwmSurveyStatusSelections + "&StillWater=" + hwmStillwaterStatusSelections;
-            //var resultIsEmpty = false;
+            // stnDataPortal.globals.xmlHWMsURLRoot = "http://stn.wim.usgs.gov/STNServices/HWMs/FilteredHWMs";
+            // stnDataPortal.globals.jsonHWMsURLRoot = "http://stn.wim.usgs.gov/STNServices/HWMs/FilteredHWMs.json";
+            // stnDataPortal.globals.csvHWMsURLRoot = "http://stn.wim.usgs.gov/STNServices/HWMs/FilteredHWMs.csv";
+            // stnDataPortal.globals.hwmsQueryString = "?Event=" + eventSelections + "&EventType=" + eventTypeSelections + "&EventStatus=" + eventStatusSelection + "&States=" + stateSelections + "&County=" + countySelections + "&HWMType=" + hwmTypeSelections + "&HWMQuality=" + hwmQualitySelections + "&HWMEnvironment=" + hwmEnvSelections + "&SurveyComplete=" + hwmSurveyStatusSelections + "&StillWater=" + hwmStillwaterStatusSelections;
+            // //var resultIsEmpty = false;
 
-            stnDataPortal.globals.csvQueryURL = stnDataPortal.globals.csvHWMsURLRoot + stnDataPortal.globals.hwmsQueryString;
-            stnDataPortal.globals.jsonQueryURL = stnDataPortal.globals.jsonHWMsURLRoot + stnDataPortal.globals.hwmsQueryString;
-            stnDataPortal.globals.xmlQueryURL = stnDataPortal.globals.xmlHWMsURLRoot + stnDataPortal.globals.hwmsQueryString;
+            // stnDataPortal.globals.csvQueryURL = stnDataPortal.globals.csvHWMsURLRoot + stnDataPortal.globals.hwmsQueryString;
+            // stnDataPortal.globals.jsonQueryURL = stnDataPortal.globals.jsonHWMsURLRoot + stnDataPortal.globals.hwmsQueryString;
+            // stnDataPortal.globals.xmlQueryURL = stnDataPortal.globals.xmlHWMsURLRoot + stnDataPortal.globals.hwmsQueryString;
 
         }
         ////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -158,15 +189,15 @@ var buildQueryStrings =  function  () {
                 peakEndDate = $("#peakEndDate")[0].value;
             }
 
-            stnDataPortal.globals.xmlPeaksURLRoot = "http://stn.wim.usgs.gov/STNServices/PeakSummaries/FilteredPeaks";
-            stnDataPortal.globals.jsonPeaksURLRoot = "http://stn.wim.usgs.gov/STNServices/PeakSummaries/FilteredPeaks.json";
-            stnDataPortal.globals.csvPeaksURLRoot = "http://stn.wim.usgs.gov/STNServices/PeakSummaries/FilteredPeaks.csv";
-            stnDataPortal.globals.peaksQueryString = "?Event=" + eventSelections + "&EventType=" + eventTypeSelections + "&EventStatus=" + eventStatusSelection + "&States=" + stateSelections + "&County=" + countySelections + "&StartDate="  + peakStartDate + "&EndDate=" + peakEndDate;
-            //var resultIsEmpty = false;
+            // stnDataPortal.globals.xmlPeaksURLRoot = "http://stn.wim.usgs.gov/STNServices/PeakSummaries/FilteredPeaks";
+            // stnDataPortal.globals.jsonPeaksURLRoot = "http://stn.wim.usgs.gov/STNServices/PeakSummaries/FilteredPeaks.json";
+            // stnDataPortal.globals.csvPeaksURLRoot = "http://stn.wim.usgs.gov/STNServices/PeakSummaries/FilteredPeaks.csv";
+            // stnDataPortal.globals.peaksQueryString = "?Event=" + eventSelections + "&EventType=" + eventTypeSelections + "&EventStatus=" + eventStatusSelection + "&States=" + stateSelections + "&County=" + countySelections + "&StartDate="  + peakStartDate + "&EndDate=" + peakEndDate;
+            // //var resultIsEmpty = false;
 
-            stnDataPortal.globals.csvQueryURL = stnDataPortal.globals.csvPeaksURLRoot + stnDataPortal.globals.peaksQueryString;
-            stnDataPortal.globals.jsonQueryURL = stnDataPortal.globals.jsonPeaksURLRoot + stnDataPortal.globals.peaksQueryString;
-            stnDataPortal.globals.xmlQueryURL = stnDataPortal.globals.xmlPeaksURLRoot + stnDataPortal.globals.peaksQueryString;
+            // stnDataPortal.globals.csvQueryURL = stnDataPortal.globals.csvPeaksURLRoot + stnDataPortal.globals.peaksQueryString;
+            // stnDataPortal.globals.jsonQueryURL = stnDataPortal.globals.jsonPeaksURLRoot + stnDataPortal.globals.peaksQueryString;
+            // stnDataPortal.globals.xmlQueryURL = stnDataPortal.globals.xmlPeaksURLRoot + stnDataPortal.globals.peaksQueryString;
 
         }
     };
