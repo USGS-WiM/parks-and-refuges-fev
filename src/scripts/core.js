603da14429ed49c53d02b5432d3efb3afe1e2943
Draper <BDRAPER@USGS.GOV>
2017-09-07 18:38:51 -0500

    changed default basemap; NWIS gages on by default, legend open by default

--- src/scripts/core.js
+++ src/scripts/core.js
@@ -279,7 +279,7 @@ $( document ).ready(function() {
 
 	/* create map */
 	map = L.map('mapDiv').setView([39.833333, -98.583333], 4);
-	var layer = L.esri.basemapLayer('Gray').addTo(map);
+	var layer = L.esri.basemapLayer('Topographic').addTo(map);
 	var layerLabels;
 	L.Icon.Default.imagePath = './images';
 
@@ -323,7 +323,9 @@ $( document ).ready(function() {
 	//add USGS rt gages to the map
 	//rdg.addTo(map);
 
-	//USGSrtGages.addTo(map);
+	//display USGS rt gages by default on map load
+	USGSrtGages.addTo(map);
+
 	//define layer 'overlays' (overlay is a leaflet term)
 	//define the real-time overlay and manually add the NWIS RT gages to it
 	var realTimeOverlays = {
