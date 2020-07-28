0eaa044aad378757ec2ca6198ff20fe68404142d
lprivette <lprivette@usgs.gov>
2020-07-28 16:07:06 -0400

    fixes bug with hwm site summaries; accounts for undefined values in the site summaries

--- src/scripts/regionalSummary.js
+++ src/scripts/regionalSummary.js
@@ -1222,7 +1222,7 @@ function displayRegionalRtGageReport(regionalStreamGages) {
             }
             for (var site in distincthwmsByPark) {
                 siteSumHWMVals.push({
-                    "site_name": distinctPeaksByPark[site].site_name,
+                    "site_name": distincthwmsByPark[site].site_name,
                     data: []
                 });
                 formattedHWMS.push({
@@ -1241,7 +1241,7 @@ function displayRegionalRtGageReport(regionalStreamGages) {
                             "Site Number": parksWithHWMs[hwm].data['Site Number'],
                             "Waterbody": parksWithHWMs[hwm].data['Waterbody']
                         };
-
+                        
                         if(parksWithHWMs[hwm].data['Elevation (ft)'] !== undefined) {
                             siteSumHWMVals[site].data.push(parksWithHWMs[hwm].data['Elevation (ft)']);
                         }    
@@ -1290,13 +1290,17 @@ function displayRegionalRtGageReport(regionalStreamGages) {
                     var dataArray;
                     dataArray = item.data.sort(function (a, b) { return a - b });
                     getSummaryStats(dataArray);
-                    peakSiteSummaries.push({ "Site Name": item.site_name ,"Type": "Peak", "Total Peaks": numReg, "Max (ft)": maxReg, "Min (ft)": minReg, "Median (ft)": medianReg, "Mean (ft)": meanReg, "Standard Dev (ft)": standReg, "90% Conf Low": confIntNinetyLow, "90% Conf High": confIntNinetyHigh });
+                    if (confIntNinetyHigh !== "NaN") {
+                        peakSiteSummaries.push({ "Site Name": item.site_name ,"Type": "Peak", "Total Peaks": numReg, "Max (ft)": maxReg, "Min (ft)": minReg, "Median (ft)": medianReg, "Mean (ft)": meanReg, "Standard Dev (ft)": standReg, "90% Conf Low": confIntNinetyLow, "90% Conf High": confIntNinetyHigh });
+                    }       
                 });
                 siteSumHWMVals.forEach(function (item, idx) {
                     var dataArray;
                     dataArray = item.data.sort(function (a, b) { return a - b });
                     getSummaryStats(dataArray);
-                    hwmSiteSummaries.push({ "Site Name": item.site_name ,"Type": "HWM", "Total HWMs": numReg, "Max (ft)": maxReg, "Min (ft)": minReg, "Median (ft)": medianReg, "Mean (ft)": meanReg, "Standard Dev (ft)": standReg, "90% Conf Low": confIntNinetyLow, "90% Conf High": confIntNinetyHigh });
+                    if (confIntNinetyHigh !== "NaN") {
+                        hwmSiteSummaries.push({ "Site Name": item.site_name ,"Type": "HWM", "Total HWMs": numReg, "Max (ft)": maxReg, "Min (ft)": minReg, "Median (ft)": medianReg, "Mean (ft)": meanReg, "Standard Dev (ft)": standReg, "90% Conf Low": confIntNinetyLow, "90% Conf High": confIntNinetyHigh });
+                    }          
                 });
             }
 
